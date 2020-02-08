f52a253b14c4613b3e64d73b3d13d3eaa2b010c9
Юрій Тарасенко <jurikfav0rit@gmail.com>
2020-02-08 20:45:33 +0200

    misc changes

---
+++ uran/Model/SearchResultsModel.swift
@@ -0,0 +1,26 @@
+//
+//  SearchResultsModel.swift
+//  uran
+//
+//  Created by Tarasenko Jurik on 08.02.2020.
+//  Copyright © 2020 Next Level. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResultsModel: Decodable {
+    let total: Int
+    let results: [UnsplashPhoto]
+}
+
+struct UnsplashPhoto: Decodable {
+    let width: Int
+    let height: Int
+    let urls: [ImageSize.RawValue: String]
+    
+    enum ImageSize: String {
+        case raw, full, regular, small, thumb
+    }
+}
+
+
---
+++ uran/SearchVC/SearchNavigationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchNavigationViewController.swift
+//  uran
+//
+//  Created by Tarasenko Jurik on 08.02.2020.
+//  Copyright © 2020 Next Level. All rights reserved.
+//
+
+import UIKit
+
+final class SearchNavigationViewController: UINavigationController {
+
+}
